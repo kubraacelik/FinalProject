c9efc84431ccf36aa83ff90b468278b45eb12e74
kubraacelik <kkbra.celik92@gmail.com>
2024-08-14 17:08:23 +0300

    created Access Token

---
+++ Core/Utilities/Security/JWT/AccessToken.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.JWT
+{
+    //kullanıcı sisteme istekte bulunduğunda yetki gerektiren bir şeyse token'ı da yollar ve buna AccessToken denir
+    public class AccessToken
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
