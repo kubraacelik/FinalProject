2d784cbc24b5b5df4a77feef212ef2576321495c
kubraacelik <kkbra.celik92@gmail.com>
2024-08-18 20:08:09 +0300

    defines the basic functionality required for the cache.

---
+++ Core/CrossCuttingConcerns/Caching/ICacheManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Caching
+{
+    public interface ICacheManager
+    {
+        //key veriyoruz bellekten key'e karşılık gelen data'yı getiriyor
+        T Get<T>(string key);
+        object Get(string key);
+        //duration = cache'de ne kadar duracak
+        void Add(string key, object value,int duration);
+        //cache'de var mı
+        bool IsAdd(string key);
+        void Remove(string key);
+        //örneğin isminde category olanları kaldır ya da ismi get ile başlayanları kaldır diyebilir
+        void RemoveByPattern(string pattern);
+    }
+}
